9289a8dfbca70ef72700d1c735406156ad309c66
Douglas <163703664+DenricoL@users.noreply.github.com>
2025-02-24 21:42:55 -0300

    Add files via upload

--- Banco de Dados 2/Aula1_SQL.sql
+++ Banco de Dados 2/Aula1_SQL.sql
@@ -1,3 +1,4 @@
+-- AULA 1 (24/02/2025)
 -- Criando o banco de dados
 CREATE DATABASE empresaCJ3025721;
 GO
@@ -30,6 +31,37 @@ INSERT INTO FUNCIONARIOS (
 VALUES (1, 'Maria da Silva', 'F', '10/01/2018', 2500.00);
 GO
 
+-- Inserindo um novo registro
 INSERT INTO FUNCIONARIOS 
 VALUES (2, 'Pedro Pereira', 'M', '25/05/2015', 990.00);
+GO
+
+/*
+Tentando inserir um funcionário em um ID já cadastrado
+INSERT INTO FUNCIONARIOS 
+VALUES (2, 'Maria Cristina', 'F', '10/09/2015', 1200.00);
+GO 
+*/
+
+-- Inserindo dois novos registros
+INSERT INTO FUNCIONARIOS 
+VALUES (3, 'Maria Cristina', 'F', '10/09/2015', 1200.00);
+GO
+INSERT INTO FUNCIONARIOS
+VALUES (4, 'Antônio Carlos', 'M', '15/05/2015', 990.00);
+GO
+
+-- Inserindo quatro novos registros (sintaxe diferente)
+INSERT INTO FUNCIONARIOS VALUES
+	(5, 'Marcelo Augusto', 'M', '09/12/2017', 1900.00),
+	(6, 'Pedro Silva', 'M', '15/11/2015', 1050.00),
+	(7, 'Mônica da Silva', 'F', '12/10/2014', 3000.00),
+	(8, 'Tiago Lima', 'M', '10/05/2016', 1350.00);
+GO
+
+SELECT ID AS 'Código do Funcionário',
+	   Nome,
+	   Sexo,
+	   Salario AS 'Salário'
+FROM FUNCIONARIOS;
 GO
