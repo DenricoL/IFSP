d5016dacbda67bd3fd1ef8e6f43f3025753f4a74
_ <163703664+DenricoL@users.noreply.github.com>
2025-09-01 21:24:26 -0300

    Add files via upload

---
+++ 4_2025.2/WEB3/Atividade1/target/classes/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO categories (name) VALUES ('Pessoal');
+INSERT INTO categories (name) VALUES ('Trabalho');
+INSERT INTO categories (name) VALUES ('Estudos');
+
+INSERT INTO users (name, email, password, phone, active) VALUES
+('Alice Silva', 'alice@ifsp.edu.br', 'senha123', '(11) 99999-1111', TRUE);
+INSERT INTO users (name, email, password, phone, active) VALUES
+('Bruno Costa', 'bruno@ifsp.edu.br', 'senha123', '(11) 99999-2222', FALSE);
